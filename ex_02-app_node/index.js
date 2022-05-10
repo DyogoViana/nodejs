125b74625fd00d42a75246942acb60f7c3954ba6
DyogoViana <dyogo.vianaa@gmail.com>
2022-05-10 15:47:47 -0300

    [ App perguntas e respostas com Node ] : : Organizando as linhas das perguntas e respostas.

--- ex_02-app_node/index.js
+++ ex_02-app_node/index.js
@@ -1,17 +1,24 @@
-const perguntas = [
+const questoes = [
 	"O que aprendi hoje?",
 	"O que me deixoou aborrecido? E o que poderia fazer para melhorar?",
 	"O que me deixou feliz hoje?",
 	"Quantas pessoas ajudei hoje?",
 ]
 
-const resposta = (index = 0) => {
-	process.stdout.write(perguntas[index] + "\n\n\n" );
+const perguntar = (index = 0) => {
+	process.stdout.write("\n\n" + questoes[index] + " > " );
 }
 
-resposta();
+perguntar();
+
+const respostas =[];
 
 process.stdin.on("data", data => {
-	process.stdout.write(data.toString().trim() + "\n") // pega os dados, converte em string e retira os espaços.
-	process.exit()
+	respostas.push(data.toString().trim())
+	if (respostas.length < questoes.length) {
+		perguntar(respostas.length);
+	} else {
+		console.log(respostas);
+		process.exit();
+	}
 })
