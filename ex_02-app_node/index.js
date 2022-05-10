a03f4308a21989705411c0620f0746377a450444
DyogoViana <dyogo.vianaa@gmail.com>
2022-05-10 15:55:23 -0300

    Fim do app de perguntas e respostas

--- ex_02-app_node/index.js
+++ ex_02-app_node/index.js
@@ -6,12 +6,12 @@ const questoes = [
 ]
 
 const perguntar = (index = 0) => {
-	process.stdout.write("\n\n" + questoes[index] + " > " );
+	process.stdout.write("\n\n" + questoes[index] + " > ");
 }
 
 perguntar();
 
-const respostas =[];
+const respostas = [];
 
 process.stdin.on("data", data => {
 	respostas.push(data.toString().trim())
@@ -21,4 +21,28 @@ process.stdin.on("data", data => {
 		console.log(respostas);
 		process.exit();
 	}
+});
+
+process.on("exit", () => {
+	console.log(`
+		Massa!!!
+
+		- O que vc aprendeu hoje foi:
+		${respostas[0]}
+
+
+		- O que te aborreceu e o que vc poderia melhorar foi:
+		${respostas[1]}
+
+
+		- Oq ue te deixou feliz hoje:
+		${respostas[2]}
+
+
+		- Você ajudou ${respostas[3]} pessoas hoje!
+
+
+
+		Volte amanhã para novas reflexões
+	`);
 })
